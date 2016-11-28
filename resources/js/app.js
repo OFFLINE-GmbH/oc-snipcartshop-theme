e5343844c842daaf5187d5684320652465084efd
Tobias Kündig <tobias@offline.swiss>
2016-11-28 09:16:10 +0100

    Layout changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
 var attachFastClick = require('fastclick');
 
-$(function() {
+$(function () {
     global.$body = $('body');
-    global.$header = $('.header');
 
-    // require('./modules/scrollHeader');
+    setTimeout(() => {
+        // SnipCart needs a few moments to populate the
+        // badge with the item count. To prevent the flash
+        // of the empty badge, we'll just show it delayed.
+        $('.cart__badge').addClass('cart__badge--visible');
+    }, 1000);
 
     attachFastClick(document.body);
 });
