0297b6836c4165d9346adb47e739b60c005728d6
Tobias Kündig <tobias@offline.swiss>
2016-11-29 21:49:22 +0100

    Integrated CustomerDashboard component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,10 @@ $(function () {
     global.$body = $('body');
 
     setTimeout(() => {
-        // SnipCart needs a few moments to populate the
-        // badge with the item count. To prevent the flash
-        // of the empty badge, we'll just show it delayed.
-        $('.cart__badge').addClass('cart__badge--visible');
+        // Snipcart needs a few moments to populate the
+        // summaries with content. To prevent the flash
+        // of the empty item, we'll just show it delayed.
+        $('.deferred-item').addClass('deferred-item--visible');
     }, 1000);
 
     attachFastClick(document.body);
